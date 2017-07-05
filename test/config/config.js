e2e3433a298f63c48b3b3c299cd5fbc5b7e26ca0
chenkang1 <chenkang1@lenovo.com>
2017-07-05 10:50:16 +0800

    remove useless

---
+++ test/config/config.js
@@ -0,0 +1,5 @@
+// console.log(__dirname)
+console.log('...............')
+
+let p = process.cwd();
+console.log(p)
---
+++ test/test.js
@@ -0,0 +1,9 @@
+let p = process.cwd();
+console.log(p)
+
+require('./config/config.js')
+
+let path = require('path')
+
+
+// console.log(path.resolve())
